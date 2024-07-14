ce68ee16f16061d2611acadf98b9c14d8bacdbad
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-07-14 17:46:29 +0900

    feat: initial portfolio release with planned updates

---
+++ src/slices/TextBlock/index.tsx
@@ -0,0 +1,22 @@
+import Bounded from "@/components/Bounded";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `TextBlock`.
+ */
+export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
+
+/**
+ * Component for "TextBlock" Slices.
+ */
+const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
+  return (
+    <div className="max-w-prose">
+      {/* <div className="prose prose-invert"></div> */}
+      <PrismicRichText field={slice.primary.text} />
+    </div>
+  );
+};
+
+export default TextBlock;
